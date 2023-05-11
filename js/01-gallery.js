b2719cdab51276749b2e687c58ec5bf271dc2892
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-05-11 22:15:25 +0300

    зміни 11/05/23 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+function renderGallery(items) {
+  const galleryMarkup = items.map(createGalleryItem).join('');
+  galleryContainer.innerHTML = galleryMarkup;
+}
+
+
+renderGallery(galleryItems);
+
 
 console.log(galleryItems);
