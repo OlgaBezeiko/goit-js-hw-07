093f05bc376582376dca6d0112e5625c6a130a88
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-05-13 19:57:23 +0300

    зміни 13/05/23 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 
-
 const galleryContainer = document.querySelector('.gallery');
 
 function createGalleryItem({ preview, original, description }) {
   return `
     <li class="gallery__item">
-      <a
-        class="gallery__link"
-        href="${original}"
-      >
+      <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
           alt="${description}"
+          data-source="${original}"
         />
       </a>
     </li>
@@ -25,8 +22,32 @@ function renderGallery(items) {
   galleryContainer.innerHTML = galleryMarkup;
 }
 
-
 renderGallery(galleryItems);
 
+galleryContainer.addEventListener('click', handleImageClick);
 
-console.log(galleryItems);
+let instance = null;
+
+function handleImageClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const originalImageURL = event.target.dataset.source;
+    const description = event.target.getAttribute('alt');
+    
+    instance = basicLightbox.create(`
+      <div class="modal">
+        <img class="modal__image" src="${originalImageURL}" alt="${description}" />
+      </div>
+    `);
+    
+    instance.show();
+    document.addEventListener('keydown', handleKeyDown);
+  }
+}
+
+function handleKeyDown(event) {
+  if (event.key === 'Escape' && instance) {
+    instance.close();
+    document.removeEventListener('keydown', handleKeyDown);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          data-source="${original}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+function renderGallery(items) {
+  const galleryMarkup = items.map(createGalleryItem).join('');
+  galleryContainer.innerHTML = galleryMarkup;
+}
+
+renderGallery(galleryItems);
+
+// Ініціалізація бібліотеки SimpleLightbox з додаванням підписів та затримкою
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+
